26a97b6b1d3724e46083a19f5e914376251144ba
Ike <gogreen37@me.com>
2015-09-18 09:48:18 -0700

    add getToppingPrice function.  Probably will not use.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,39 @@
-debugger;
-function Pizza (toppings, pizza_size, quantity) {
-    this.toppings = toppings;
+function Pizza (topping, pizza_size, quantity) {
+    this.topping = topping;
     this.pizza_size = pizza_size;
     this.quantity = quantity;
+}
+
+Pizza.prototype.getToppingPrice = function(topping) {
+    var topping = this.topping;
+    var toppings = {
+        cheese : 0,
+        meat : 2,
+        veg : 1,
+    }
+
+
+    var topping_price = toppings.topping;
+    console.log(topping_price)
+
+}
+
+Pizza.prototype.getPrice = function(topping, pizza_size, quantity){
+    var toppings = {
+        cheese : 0,
+        meat : 2,
+        veg : 1,
+    }
+
+    var sizes = {
+        small : 8,
+        medium : 10,
+        large : 12,
+        xl : 14,
+    }
+    console.log(toppings.veg)
+
+    var total_price = ((toppings.topping + sizes.pizza_size) * quantity)
+
+    return total_price;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -3,16 +3,23 @@
 describe('Pizza', function(){
     it('creates an object with toppings, pizza_size, and quantity', function() {
         var testPizza = new Pizza ('cheese', 'large', '5')
-        expect(testPizza.toppings).to.equal('cheese');
+        expect(testPizza.topping).to.equal('cheese');
         expect(testPizza.pizza_size).to.equal('large');
         expect(testPizza.quantity).to.equal('5');
     })
 
-    it'returns a price for a pizza based on toppings, pizza_size, and quantity.', function() {
-        var testPizza = new Pizza ('veg', 'xl', '1')
-        var total_price = testPizza.getPrice(toppings, pizza_size, quantity)
-        expect(testPizza.getPrice).to.equal('15')
-    }
+    it('returns a price for a pizza based on toppings, pizza_size, and quantity.', function() {
+        var testPizza = new Pizza ('veg', 'xl', '2')
+        var total_price = testPizza.getPrice('veg', 'xl', '2')
+        expect(total_price).to.equal(30)
+    })
+
+    it('returns a price for a pizza based on toppings, pizza_size, and quantity.', function() {
+        var testPizza = new Pizza ('veg', 'xl', '2')
+        var total_price = testPizza.getToppingPrice('veg')
+        expect(total_price).to.equal(1)
+    })
+
 })
 //create pizza protoype that gets price
 //create html page that accepts input
