6a366bce37ba04337d82a347ab0bc7c3b9524740
Ike <gogreen37@me.com>
2015-09-18 08:59:40 -0700

    add spec for getPrice prototype.

--- spec/specs.js
+++ spec/specs.js
@@ -7,6 +7,12 @@ describe('Pizza', function(){
         expect(testPizza.pizza_size).to.equal('large');
         expect(testPizza.quantity).to.equal('5');
     })
+
+    it'returns a price for a pizza based on toppings, pizza_size, and quantity.', function() {
+        var testPizza = new Pizza ('veg', 'xl', '1')
+        var total_price = testPizza.getPrice(toppings, pizza_size, quantity)
+        expect(testPizza.getPrice).to.equal('15')
+    }
 })
 //create pizza protoype that gets price
 //create html page that accepts input
