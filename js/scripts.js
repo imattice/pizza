38118c4e851ed179609139098e48a4f4850a8db2
Ike <gogreen37@me.com>
2015-09-18 08:50:53 -0700

    add Pizza object to t to scripts.js.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+debugger;
+function Pizza (toppings, pizza_size, quantity) {
+    this.toppings = toppings;
+    this.pizza_size = pizza_size;
+    this.quantity = quantity;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -3,9 +3,9 @@
 describe('Pizza', function(){
     it('creates an object with toppings, pizza_size, and quantity', function() {
         var testPizza = new Pizza ('cheese', 'large', '5')
-        expect(Pizza.toppings).to.equal('cheese');
-        expect(Pizza.pizza_size).to.equal('large');
-        expect(Pizza.quantity).to.equal('5');
+        expect(testPizza.toppings).to.equal('cheese');
+        expect(testPizza.pizza_size).to.equal('large');
+        expect(testPizza.quantity).to.equal('5');
     })
 })
 //create pizza protoype that gets price
