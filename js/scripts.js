058aaefee82e58d692252bc56bc671bea7d440cd
Ike <gogreen37@me.com>
2015-09-18 12:28:20 -0700

    add pizza.html.

--- js/scripts.js
+++ js/scripts.js
@@ -4,36 +4,59 @@ function Pizza (topping, pizza_size, quantity) {
     this.quantity = quantity;
 }
 
-Pizza.prototype.getToppingPrice = function(topping) {
-    var topping = this.topping;
-    var toppings = {
-        cheese : 0,
-        meat : 2,
-        veg : 1,
-    }
-
-
-    var topping_price = toppings.topping;
-    console.log(topping_price)
-
-}
+// Pizza.prototype.getToppingPrice = function(topping) {
+//     var topping = this.topping;
+//     var toppings = {
+//         cheese : 0,
+//         meat : 2,
+//         veg : 1,
+//     }
+//
+//
+//     var topping_price = toppings.topping;
+//     console.log(topping_price)
+//
+// }
 
 Pizza.prototype.getPrice = function(topping, pizza_size, quantity){
-    var toppings = {
-        cheese : 0,
-        meat : 2,
-        veg : 1,
+    var topping_price;
+    var size_price;
+
+    if (topping == 'cheese') {
+        topping_price = 0;
+    } else if (topping == 'meat') {
+        topping_price = 2;
+    } else if (topping == 'veg'){
+        topping_price = 1;
     }
 
-    var sizes = {
-        small : 8,
-        medium : 10,
-        large : 12,
-        xl : 14,
+    if (pizza_size == 'small') {
+        size_price = 8;
+    } else if (pizza_size == 'meduim') {
+        size_price = 10;
+    } else if (pizza_size == 'large'){
+        size_price = 12;
+    } else if (pizza_size == 'xl'){
+        size_price = 14;
     }
-    console.log(toppings.veg)
 
-    var total_price = ((toppings.topping + sizes.pizza_size) * quantity)
+
+//use local objects within prototypes to get topping prices?
+    // var toppings = {
+    //     cheese : 0,
+    //     meat : 2,
+    //     veg : 1,
+    // }
+    //
+    // var sizes = {
+    //     small : 8,
+    //     medium : 10,
+    //     large : 12,
+    //     xl : 14,
+    // }
+    // console.log(toppings.veg)
+
+    var total_price = ((topping_price + size_price) * quantity)
 
     return total_price;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -13,13 +13,6 @@ describe('Pizza', function(){
         var total_price = testPizza.getPrice('veg', 'xl', '2')
         expect(total_price).to.equal(30)
     })
-
-    it('returns a price for a pizza based on toppings, pizza_size, and quantity.', function() {
-        var testPizza = new Pizza ('veg', 'xl', '2')
-        var total_price = testPizza.getToppingPrice('veg')
-        expect(total_price).to.equal(1)
-    })
-
 })
 //create pizza protoype that gets price
 //create html page that accepts input
