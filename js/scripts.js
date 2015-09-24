c4d110888d2d9cf212a70bf6110951b094dc3c39
Ike <gogreen37@me.com>
2015-09-24 16:52:49 -0700

    refactor to remove input from .getPrice() and rely on Pizza object.

--- js/scripts.js
+++ js/scripts.js
@@ -4,59 +4,29 @@ function Pizza (topping, pizza_size, quantity) {
     this.quantity = quantity;
 }
 
-// Pizza.prototype.getToppingPrice = function(topping) {
-//     var topping = this.topping;
-//     var toppings = {
-//         cheese : 0,
-//         meat : 2,
-//         veg : 1,
-//     }
-//
-//
-//     var topping_price = toppings.topping;
-//     console.log(topping_price)
-//
-// }
-
-Pizza.prototype.getPrice = function(topping, pizza_size, quantity){
+Pizza.prototype.getPrice = function(){
     var topping_price;
     var size_price;
 
-    if (topping == 'cheese') {
+    if (this.topping == 'cheese') {
         topping_price = 0;
-    } else if (topping == 'meat') {
+    } else if (this.topping == 'meat') {
         topping_price = 2;
-    } else if (topping == 'veg'){
+    } else if (this.topping == 'veg'){
         topping_price = 1;
     }
 
-    if (pizza_size == 'small') {
+    if (this.pizza_size == 'small') {
         size_price = 8;
-    } else if (pizza_size == 'medium') {
+    } else if (this.pizza_size == 'medium') {
         size_price = 10;
-    } else if (pizza_size == 'large'){
+    } else if (this.pizza_size == 'large'){
         size_price = 12;
-    } else if (pizza_size == 'xl'){
+    } else if (this.pizza_size == 'xl'){
         size_price = 14;
     }
 
-
-//use local objects within prototypes to get topping prices?
-    // var toppings = {
-    //     cheese : 0,
-    //     meat : 2,
-    //     veg : 1,
-    // }
-    //
-    // var sizes = {
-    //     small : 8,
-    //     medium : 10,
-    //     large : 12,
-    //     xl : 14,
-    // }
-    // console.log(toppings.veg)
-
-    var total_price = ((topping_price + size_price) * quantity)
+    var total_price = ((topping_price + size_price) * this.quantity)
 
     return total_price;
 }
@@ -73,8 +43,7 @@ $(document).ready(function() {
         var new_size = $('select#new_size').val();
         var new_quantity = $('input#new_quantity').val();
         var newPizza = new Pizza (new_topping, new_size, new_quantity);
-        console.log(newPizza);
-        var total_price = newPizza.getPrice(new_topping, new_size, new_quantity)
+        var total_price = newPizza.getPrice()
 
         $('span#total_price').text(total_price);
         $('div#reciept').show();
